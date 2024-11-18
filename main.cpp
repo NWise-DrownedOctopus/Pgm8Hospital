87b873f8c489201f18e001c3b1832fbb4246c8bd
Nicholas Wise <nicholasmwise@gmail.com>
2024-11-18 10:40:19 -0600

    Opened file, and printed lines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream inputFile("patient.csv");
+
+    string line;
+
+    if (!inputFile.is_open()) {
+        cout << "Error opening file" << endl;
+        return -1;
+    }
+
+    while (getline(inputFile, line)) {
+        cout << line << endl;
+    }
 }
